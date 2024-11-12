8c761ef8ccefce1639554c9464804655ad9e281c
Aditya Adhav <superboi611@gmail.com>
2024-11-12 21:03:17 +0530

    initial commit

---
+++ src/Scripts/reviews.js
@@ -0,0 +1,27 @@
+export const customerReviews = [
+    {
+      "name": "Shadab Perwez",
+      "location": "Sanpada Mumbai",
+      "message": "Med Plus transformed my healthcare experience! The online consultations were so convenient, and the doctors were knowledgeable and caring."
+    },
+    {
+      "name": "Aditya Adhav",
+      "location": "Accra, Ghanna",
+      "message": "I found the perfect specialist for my condition through Med Plus. The personalized treatment plan made all the difference. Thank you for prioritizing my health!"
+    },
+    {
+      "name": "Govind Gonnade",
+      "location": "Gadchiroli, Maharastra",
+      "message": "Booking appointments was a breeze, and the service exceeded my expectations. The doctors truly listen and provide effective solutions. 5 stars!"
+    },
+    {
+      "name": "Parikshit Abuj",
+      "location": "Beed, Maharastra",
+      "message": "Med Plus is a game-changer! The emergency care saved me during a critical situation. Grateful for their quick and efficient support."
+    },
+    {
+      "name": "Samrudhi Nandanwar",
+      "location": "Pune, Maharastra",
+      "message": "I used to dread dental visits, but Med Plus made it a pleasant experience. The dentist was gentle and professional. I'll definitely be back!"
+    },
+];
