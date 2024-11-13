c4355c7b7eacbdd66c77061e158d0f9b1fc03abf
atg-kong <132819625+atg-kong@users.noreply.github.com>
2024-11-13 08:04:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
   return (
     <div className="App">
     <BrowserRouter>
-    <Home/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/legal" element={<Legal />} />
